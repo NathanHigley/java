8a7c1d7c31af309956ae2f3e47d465da4d4e7005
NathanHigley <nathan@jhigley.com>
2018-05-08 09:52:48 -0700

    rebound

--- rebound/Rebound.java
+++ rebound/Rebound.java
@@ -3,20 +3,19 @@ import java.awt.event.*;
 import javax.swing.*;
 
 @SuppressWarnings("serial")
-public class rebound extends JFrame {
+public class Rebound extends JFrame {
 // CONSTANTS
    private static final int CANVAS_WIDTH = 800;
    private static final int CANVAS_HEIGHT = 800;
-   private static final int UPDATE_PERIOD = 25; // refresh by ms
+   private static final int UPDATE_PERIOD = 35; // refresh by ms
 
    private DrawCanvas canvas;
-
 // BALL ATTRIBUTES
    private int x = 100, y = 100;  // from top-left (x, y)
    private int size = 50;        // width and height
-   private int xSpeed = 5, ySpeed = 10; // displacement per step in x, y
+   private int xSpeed = 12, ySpeed = 20; // displacement per step in x, y
 
-   public rebound() {
+   public Rebound() {
       canvas = new DrawCanvas();
       canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
       this.setContentPane(canvas);
@@ -38,15 +37,16 @@ public class rebound extends JFrame {
    public void update() {
       x += xSpeed;
       y += ySpeed;
-      if (x > CANVAS_WIDTH - size || x < 0) { // hits vert wall
+      if (x > CANVAS_WIDTH - size || x < 0) { // hits vertical wall
          xSpeed = -xSpeed; // reverse
       }
-      if (y > CANVAS_HEIGHT - size || y < 0) { // hits hori wall
+      if (y > CANVAS_HEIGHT - size || y < 0) { // hits horizontal wall
          ySpeed = -ySpeed; // reverse
       }
-      if (x > 275 && x < 475 && y > 275 && y < 475) { // intersects center
-        xSpeed = -xSpeed - 2; // reverse, increase speed
-        ySpeed = -ySpeed - 2;
+      //System.out.println(x+" "+y+" "+xSpeed+" "+ySpeed); // print ball coordinates
+      if (x > 275 && x < 475 && y > 275 && y < 475 && xSpeed <= 50 && ySpeed <= 50) { // intersects center
+        xSpeed = -xSpeed * 100 / 90; // reverse, increase speed by factor
+        ySpeed = -ySpeed * 100 / 90;
       }
    }
 // DESIGN
@@ -56,8 +56,8 @@ public class rebound extends JFrame {
          super.paintComponent(g);
          setBackground(Color.BLACK);
          //Color c = new Color((int)(Math.random() * 0xFFFFFF)); // randomize colors
-         g.setColor(Color.WHITE); // STATIC COLOR
          //g.setColor(c); // RANDOM COLOR
+         g.setColor(Color.WHITE); // STATIC COLOR
          g.fillOval(x, y, size, size); // BALL SHAPE
          g.fillOval(300, 300, 200, 200); // CENTER SHAPE
       }
@@ -67,7 +67,7 @@ public class rebound extends JFrame {
       SwingUtilities.invokeLater(new Runnable() {
          @Override
          public void run() {
-            new rebound();
+            new Rebound();
          }
       });
    }
