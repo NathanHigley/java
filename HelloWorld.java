480c5b58f482a706d4f1ba3818a5cf7c00ffd5d6
NathanHigley <nathan@jhigley.com>
2018-11-07 09:13:07 -0800

    oo

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
   public static void main (String args[]){
     System.out.println( " HELLO WORLD");
-    System.out.println( " NEVER FORGET ;");
+    System.out.println( "  NEVER FORGET ;");
   }
 }
